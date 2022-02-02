308cc8aa1d3efa896fb450c1d5f11139911542b6
Th1nkK1D <Th1nkK1D@users.noreply.github.com>
2022-02-02 17:35:01 +0000

    deploy: d2faf86201e9fcedc40a59d4a873397729a2a533

--- reconstitution/_nuxt/static/1643820827/categories/reform/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Freconstitution\u002Freconstitution\u002F_nuxt\u002Fstatic\u002F1643820827",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcategories\u002Freform",config:{app:{basePath:"\u002Freconstitution\u002F",assetsPath:"\u002Freconstitution\u002Freconstitution\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ reconstitution/_nuxt/static/1643823277/categories/political_ethics/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Freconstitution\u002Freconstitution\u002F_nuxt\u002Fstatic\u002F1643823277",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcategories\u002Fpolitical_ethics",config:{app:{basePath:"\u002Freconstitution\u002F",assetsPath:"\u002Freconstitution\u002Freconstitution\u002F_nuxt\u002F",cdnURL:a}}}}(null));

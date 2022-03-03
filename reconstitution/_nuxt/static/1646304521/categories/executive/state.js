6f618cdeb4ac6d93e2ca49069c006a367b8c045d
Sitthikarn-karn <Sitthikarn-karn@users.noreply.github.com>
2022-03-03 10:49:07 +0000

    deploy: 621d10573f696b18d918b99011ae8ee32a07e6bc

---
+++ reconstitution/_nuxt/static/1646304521/categories/executive/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Freconstitution\u002Freconstitution\u002F_nuxt\u002Fstatic\u002F1646304521",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcategories\u002Fexecutive",config:{app:{basePath:"\u002Freconstitution\u002F",assetsPath:"\u002Freconstitution\u002Freconstitution\u002F_nuxt\u002F",cdnURL:a}}}}(null));

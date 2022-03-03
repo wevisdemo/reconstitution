5ddfddbc5d7d22ec504530f3dd8a919f62e7a91e
Sitthikarn-karn <Sitthikarn-karn@users.noreply.github.com>
2022-03-03 11:18:26 +0000

    deploy: 621d10573f696b18d918b99011ae8ee32a07e6bc

--- reconstitution/_nuxt/static/1646304521/categories/political_ethics/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Freconstitution\u002Freconstitution\u002F_nuxt\u002Fstatic\u002F1646304521",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcategories\u002Fpolitical_ethics",config:{app:{basePath:"\u002Freconstitution\u002F",assetsPath:"\u002Freconstitution\u002Freconstitution\u002F_nuxt\u002F",cdnURL:a}}}}(null));
